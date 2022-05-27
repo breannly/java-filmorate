2dfbd8b4df4b0015e417dea4b482f7868fbe57aa
Armen Katvalyan <aakatvalyan@bk.ru>
2022-05-27 18:12:33 +0300

    feat: добавить класс User

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.filmorate.model;
+
+import java.time.LocalDateTime;
+
+public class User {
+    private Long id;
+    private String email;
+    private String login;
+    private String name;
+    private LocalDateTime birthday;
+}
