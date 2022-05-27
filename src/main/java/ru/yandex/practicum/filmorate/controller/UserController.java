b5240d8d3de9258b09b6bf09891fb010495d3408
Armen Katvalyan <aakatvalyan@bk.ru>
2022-05-28 00:16:32 +0300

    refactor: дополнить UserController

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,34 +1,77 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.generator.GeneratorUserId;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
+import java.util.*;
 
-@RestController("/users")
+@RestController
+@RequestMapping("/users")
+@Slf4j
 public class UserController {
-    List<User> users = new ArrayList<>();
+    private final Map<Long, User> users;
+    private final GeneratorUserId generatorId;
+
+    public UserController() {
+        users = new HashMap<>();
+        generatorId = new GeneratorUserId();
+    }
 
     @GetMapping
-    public List<User> getUsers() {
-        return users;
+    public Collection<User> getUsers(HttpServletRequest request) {
+        log.info("Получен запрос к эндпоинту: '{} {}'", request.getMethod(), request.getRequestURI());
+
+        return users.values();
     }
 
     @PostMapping
-    public User addUser(@RequestBody User user) {
-        users.add(user);
+    public User addUser(@RequestBody User user, HttpServletRequest request) throws ValidationException {
+        log.info("Получен запрос к эндпоинту: '{} {}'", request.getMethod(), request.getRequestURI());
+        checkValidation(user);
+
+        users.put(createUser(user), user);
+        return user;
+    }
+
+    private Long createUser(User user) {
+        user.setId(generatorId.generate());
+
+        return user.getId();
+    }
+
+    @PutMapping
+    public User updateUser(@RequestBody User user, HttpServletRequest request) throws ValidationException {
+        log.info("Получен запрос к эндпоинту: '{} {}'", request.getMethod(), request.getRequestURI());
+        checkValidation(user);
+
+        if (!users.containsKey(user.getId())) {
+            log.info("Неверно указан id");
+            throw new ValidationException("Пользователя с таким id нет");
+        }
+        users.put(user.getId(), user);
 
         return user;
     }
 
-    @PutMapping
-    public User updateUser(@RequestBody User user) {
-        if (users.contains(user)) {
-            users.remove(user);
-        }
-        users.add(user);
+    private void checkValidation(User user) throws ValidationException {
+        boolean isWrongEmail = user.getEmail().isBlank() || !user.getEmail().contains("@");
+        boolean isWrongLogin = user.getLogin().isBlank() || user.getLogin().contains(" ");
+        boolean isWrongName = user.getName().isBlank();
+        boolean isWrongBirthday = user.getBirthday().isAfter(LocalDate.now());
 
-        return user;
+        if (isWrongEmail || isWrongLogin || isWrongBirthday) {
+            log.info("Запрос не прошел User-валидацию");
+            throw new ValidationException("Пользователь не соответствует критериям");
+        }
+
+        if (isWrongName) {
+            log.info("Имя изменено на логин");
+            user.setName(user.getLogin());
+        }
     }
 }
