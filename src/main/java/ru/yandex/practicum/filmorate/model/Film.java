c572d067dd50f8f292cb95ad7ced22eed6d5c0c8
Armen Katvalyan <aakatvalyan@bk.ru>
2022-05-27 18:12:10 +0300

    feat: добавить класс Film

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,10 @@
+package ru.yandex.practicum.filmorate.model;
+
+import java.time.LocalDateTime;
+
+public class Film {
+    private Long id;
+    private String description;
+    private LocalDateTime releaseDate;
+    private long duration;
+}
