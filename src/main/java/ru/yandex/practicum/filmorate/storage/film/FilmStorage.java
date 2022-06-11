9b5e0b6c1e116f9c242ac46739d3623aa6198ef6
Armen Katvalyan <aakatvalyan@bk.ru>
2022-06-12 02:56:40 +0300

    feat: добавить хранилище film

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -1,4 +1,14 @@
 package ru.yandex.practicum.filmorate.storage.film;
 
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.Collection;
+
 public interface FilmStorage {
+    public Collection<Film> findAll();
+
+    public Film add(Film film);
+
+    public Film update(Film film) throws ValidationException;
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -1,4 +1,51 @@
 package ru.yandex.practicum.filmorate.storage.film;
 
-public class InMemoryFilmStorage {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.generator.FilmIdGenerator;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Component
+public class InMemoryFilmStorage implements FilmStorage {
+    private final Map<Long, Film> films;
+    private final FilmIdGenerator generator;
+
+    @Autowired
+    public InMemoryFilmStorage(FilmIdGenerator generator) {
+        films = new HashMap<>();
+        this.generator = generator;
+    }
+
+    @Override
+    public Collection<Film> findAll() {
+        return films.values();
+    }
+
+    @Override
+    public Film add(Film film) {
+        film.setId(generator.generate());
+        films.put(film.getId(), film);
+        log.info("Объект {} добавлен", film);
+
+        return film;
+    }
+
+    @Override
+    public Film update(Film film) {
+        if (!films.containsKey(film.getId())) {
+            log.warn("Объект {} с id {} не найден", film, film.getId());
+            throw new ValidationException("Обновление несуществующего объекта");
+        }
+        films.put(film.getId(), film);
+        log.info("Объект {} изменен", film);
+
+        return film;
+    }
 }
