03309ea2b55febc8310ee0de60ce0448bca96767
Armen Katvalyan <aakatvalyan@bk.ru>
2022-06-12 02:57:45 +0300

    feat: добавить хранилище user

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
@@ -1,4 +1,51 @@
 package ru.yandex.practicum.filmorate.storage.user;
 
-public class InMemoryUserStorage {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.generator.UserIdGenerator;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Component
+public class InMemoryUserStorage implements UserStorage {
+    private final Map<Long, User> users;
+    private final UserIdGenerator generator;
+
+    @Autowired
+    public InMemoryUserStorage(UserIdGenerator generator) {
+        users = new HashMap<>();
+        this.generator = generator;
+    }
+
+    @Override
+    public Collection<User> findAll() {
+        return users.values();
+    }
+
+    @Override
+    public User add(User user) {
+        user.setId(generator.generate());
+        users.put(user.getId(), user);
+        log.info("Объект {} добавлен", user);
+
+        return user;
+    }
+
+    @Override
+    public User update(User user) {
+        if (!users.containsKey(user.getId())) {
+            log.warn("Объект {} с id {} не найден", user, user.getId());
+            throw new ValidationException("Обновление несуществующего объекта");
+        }
+        users.put(user.getId(), user);
+        log.info("Объект {} изменен", user);
+
+        return user;
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -1,4 +1,15 @@
 package ru.yandex.practicum.filmorate.storage.user;
 
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.Collection;
+
+@Component
 public interface UserStorage {
+    public Collection<User> findAll();
+
+    public User add(User user);
+
+    public User update(User user);
 }
