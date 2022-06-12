d48f53d133f62109d42180296657f8952fedf912
Armen Katvalyan <aakatvalyan@bk.ru>
2022-06-12 03:04:04 +0300

    refactor: добавить зависимость хранилища в контроллер

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,9 +1,12 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
 
 import javax.validation.Valid;
 import java.util.Collection;
@@ -13,24 +16,32 @@ import static ru.yandex.practicum.filmorate.config.Config.validateDate;
 @Slf4j
 @RestController
 @RequestMapping("/films")
-public class FilmController extends AbstractController<Film> {
+public class FilmController {
+    private final FilmStorage inMemoryFilmStorage;
+
+    @Autowired
+    public FilmController(InMemoryFilmStorage inMemoryFilmStorage) {
+        this.inMemoryFilmStorage = inMemoryFilmStorage;
+    }
+
     @GetMapping
     public Collection<Film> findAll() {
-        return super.findAll();
+        return inMemoryFilmStorage.findAll();
     }
 
     @PostMapping
-    public Film add(@Valid @RequestBody Film film) throws ValidationException {
-        return super.add(film);
+    public Film add(@Valid @RequestBody Film film) {
+        validate(film);
+        return inMemoryFilmStorage.add(film);
     }
 
     @PutMapping
-    public Film update(@Valid @RequestBody Film film) throws ValidationException {
-        return super.update(film);
+    public Film update(@Valid @RequestBody Film film) {
+        validate(film);
+        return inMemoryFilmStorage.update(film);
     }
 
-    @Override
-    protected void validate(Film film) throws ValidationException {
+    private void validate(Film film) {
         boolean isWrongReleaseDate = film.getReleaseDate().isBefore(validateDate);
 
         if (isWrongReleaseDate) {
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,9 +1,12 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import javax.validation.Valid;
 import java.util.Collection;
@@ -11,24 +14,32 @@ import java.util.Collection;
 @Slf4j
 @RestController
 @RequestMapping("/users")
-public class UserController extends AbstractController<User> {
+public class UserController {
+    private final UserStorage inMemoryUserStorage;
+
+    @Autowired
+    public UserController(InMemoryUserStorage inMemoryUserStorage) {
+        this.inMemoryUserStorage = inMemoryUserStorage;
+    }
+
     @GetMapping
     public Collection<User> findAll() {
-        return super.findAll();
+        return inMemoryUserStorage.findAll();
     }
 
     @PostMapping
-    public User add(@Valid @RequestBody User user) throws ValidationException {
-        return super.add(user);
+    public User add(@Valid @RequestBody User user) {
+        validate(user);
+        return inMemoryUserStorage.add(user);
     }
 
     @PutMapping
-    public User update(@Valid @RequestBody User user) throws ValidationException {
-        return super.update(user);
+    public User update(@Valid @RequestBody User user) {
+        validate(user);
+        return inMemoryUserStorage.update(user);
     }
 
-    @Override
-    protected void validate(User user) {
+    private void validate(User user) {
         boolean isWrongName = user.getName().isBlank();
 
         if (isWrongName) {
