1fa46ca78228bc48181167ce82818e977dc70bf6
Armen Katvalyan <aakatvalyan@bk.ru>
2022-06-14 13:47:00 +0300

    feat: добавить FilmService

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
 
 import javax.validation.Valid;
 import java.util.Collection;
+import java.util.List;
 
 import static ru.yandex.practicum.filmorate.config.Config.validateDate;
 
@@ -18,10 +20,12 @@ import static ru.yandex.practicum.filmorate.config.Config.validateDate;
 @RequestMapping("/films")
 public class FilmController {
     private final FilmStorage inMemoryFilmStorage;
+    private final FilmService service;
 
     @Autowired
-    public FilmController(InMemoryFilmStorage inMemoryFilmStorage) {
+    public FilmController(InMemoryFilmStorage inMemoryFilmStorage, FilmService service) {
         this.inMemoryFilmStorage = inMemoryFilmStorage;
+        this.service = service;
     }
 
     @GetMapping
@@ -48,5 +52,25 @@ public class FilmController {
             log.warn("Слишком раняя дата релиза");
             throw new ValidationException("Слишком ранняя дата релиза");
         }
+    }
+
+    @GetMapping("/{id}")
+    public Film getFilmById(@PathVariable Long id) {
+        return service.findFilmById(id);
+    }
+
+    @PutMapping("/{id}/like/{userId}")
+    public void addLike(@PathVariable Long id, @PathVariable Long userId) {
+        service.addLike(id, userId);
+    }
+
+    @DeleteMapping("/{id}/like/{userId}")
+    public void deleteLike(@PathVariable Long id, @PathVariable Long userId) {
+        service.deleteLike(id, userId);
+    }
+
+    @GetMapping("/popular")
+    public List<Film> getFilms(@RequestParam(defaultValue = "10") int count) {
+        return service.getFilms(count);
     }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -0,0 +1,53 @@
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.ObjectNotFoundException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+public class FilmService {
+    private final FilmStorage storage;
+
+    @Autowired
+    public FilmService(FilmStorage storage) {
+        this.storage = storage;
+    }
+
+    public Film findFilmById(Long id) {
+        Film film = storage.getFilms().get(id);
+        if (film == null) {
+            log.info("Фильм {} не найден", id);
+            throw new ObjectNotFoundException("Вызов несуществующего объекта");
+        }
+
+        return film;
+    }
+
+    public void addLike(Long id, Long filmId) {
+        Film film = findFilmById(filmId);
+        film.getLikes().add(id);
+        log.info("Пользователь {} поставил лайк фильму {}", id, filmId);
+    }
+
+    public void deleteLike(Long id, Long filmId) {
+        Film film = findFilmById(filmId);
+        film.getLikes().remove(id);
+        log.info("Пользователь {} удалил лайк у фильма {}", id, filmId);
+    }
+
+    public List<Film> getFilms(int count) {
+        log.info("Получение {} фильмов", count);
+        return storage.getFilms().values()
+                .stream()
+                .sorted((x, y) -> x.getLikes().size() - y.getLikes().size())
+                .limit(count)
+                .collect(Collectors.toList());
+    }
+}
