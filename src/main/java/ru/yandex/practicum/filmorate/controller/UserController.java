8c172bee15f47ed6bfe1a6729f7a9babc5539530
Armen Katvalyan <aakatvalyan@bk.ru>
2022-06-14 13:47:41 +0300

    feat: добавить UserService

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -3,23 +3,26 @@ package ru.yandex.practicum.filmorate.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
 import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import javax.validation.Valid;
 import java.util.Collection;
+import java.util.List;
 
 @Slf4j
 @RestController
 @RequestMapping("/users")
 public class UserController {
     private final UserStorage inMemoryUserStorage;
+    private final UserService service;
 
     @Autowired
-    public UserController(InMemoryUserStorage inMemoryUserStorage) {
+    public UserController(InMemoryUserStorage inMemoryUserStorage, UserService service) {
         this.inMemoryUserStorage = inMemoryUserStorage;
+        this.service = service;
     }
 
     @GetMapping
@@ -46,5 +49,30 @@ public class UserController {
             log.info("У пользователя {} user изменено имя на {}", user, user.getLogin());
             user.setName(user.getLogin());
         }
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return service.findUserById(id);
+    }
+
+    @PutMapping("/{id}/friends/{friendId}")
+    public void addFriend(@PathVariable Long id, @PathVariable Long friendId) {
+        service.addFriend(id, friendId);
+    }
+
+    @DeleteMapping("{id}/friends/{friendId}")
+    public void deleteFriend(@PathVariable Long id, @PathVariable Long friendId) {
+        service.deleteFriend(id, friendId);
+    }
+
+    @GetMapping("{id}/friends")
+    public List<User> getFriends(@PathVariable Long id) {
+        return service.getFriends(id);
+    }
+
+    @GetMapping("{id}/friends/common/{otherId}")
+    public List<User> getFriends(@PathVariable Long id, @PathVariable Long otherId) {
+        return service.getMutualFriends(id, otherId);
     }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -0,0 +1,84 @@
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.ObjectNotFoundException;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+public class UserService {
+    private final UserStorage storage;
+
+    @Autowired
+    public UserService(InMemoryUserStorage storage) {
+        this.storage = storage;
+    }
+
+    public void addFriend(Long id, Long friendId) {
+        User user = findUserById(id);
+        User userFriend  = findUserById(friendId);
+
+        user.getFriends().add(friendId);
+        userFriend.getFriends().add(id);
+    }
+
+    public User findUserById(Long id) {
+        User user = storage.getUsers().get(id);
+        if (user == null) {
+            log.info("Пользователь {} не найден", id);
+            throw new ObjectNotFoundException("Вызов несуществующего объекта");
+        }
+
+        return user;
+    }
+
+    public void deleteFriend(Long id, Long friendId) {
+        User user = findUserById(id);
+        User userFriend  = findUserById(friendId);
+        log.info("Удаление из друзей {} {}", id, friendId);
+
+        user.getFriends().remove(friendId);
+        userFriend.getFriends().remove(id);
+    }
+
+    public List<User> getFriends(Long id) {
+        User user = findUserById(id);
+        log.info("Получение списка друзей {}", id);
+
+        return user.getFriends().stream()
+                .map(this::findUserById).
+                collect(Collectors.toList());
+    }
+
+    public List<User> getMutualFriends(Long id, Long otherId) {
+        // используется метод двух указателей, сложность алгоритма O(n)
+        List<User> mutualFriends = new ArrayList<>();
+        List<Long> idFriends = new ArrayList<>(findUserById(id).getFriends());
+        List<Long> otherIdFriends = new ArrayList<>(findUserById(otherId).getFriends());
+
+        int fPointer = 0;
+        int sPointer = 0;
+        while (fPointer < idFriends.size() && sPointer < otherIdFriends.size()) {
+            if (idFriends.get(fPointer) < otherIdFriends.get(sPointer)) {
+                fPointer++;
+            } else if (idFriends.get(fPointer) > otherIdFriends.get(sPointer)) {
+                sPointer++;
+            } else {
+                mutualFriends.add(findUserById(idFriends.get(fPointer)));
+                fPointer++;
+                sPointer++;
+            }
+        }
+        log.info("Вывод общих друзей {} и {}", id, otherId);
+
+        return mutualFriends;
+    }
+}
