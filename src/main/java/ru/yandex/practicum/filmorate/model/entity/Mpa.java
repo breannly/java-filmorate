53fbdc04921174d931704cb0fc0ad036b9ee2749
Armen Katvalyan <aakatvalyan@bk.ru>
2022-07-08 01:55:13 +0300

    feat: добавить новые сущности Mpa и Genre

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/entity/Mpa.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.filmorate.model.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Mpa {
+    private Long id;
+    private String name;
+    private String description;
+}
