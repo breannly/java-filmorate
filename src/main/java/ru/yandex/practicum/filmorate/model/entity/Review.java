98cc2802139bc53438470bd9c2e26086d454daf9
Armen Katvalyan <aakatvalyan@bk.ru>
2022-07-23 04:35:11 +0300

    refactor: добавить валидацию сущности review

--- src/main/java/ru/yandex/practicum/filmorate/model/entity/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/entity/Review.java
@@ -3,13 +3,25 @@ package ru.yandex.practicum.filmorate.model.entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class Review {
     private Long reviewId;
+
+    @NotBlank
     private String content;
+
+    @NotNull
     private Boolean isPositive;
+
+    @NotNull
     private Long userId;
+
+    @NotNull
     private Long filmId;
-    private Integer useful;
+
+    private int useful;
 }
